df3f7ff5ace16bfc50c029057a3b7081a151001a
Isnu Munandar <isnu.mdr@gmail.com>
2025-03-21 14:30:19 +0700

    bug fix deployment

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -24,6 +24,7 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-expressions": "off",
       "@typescript-eslint/no-unused-vars": "off",
+      "react/prop-types": "off",
     },
   },
 ];
